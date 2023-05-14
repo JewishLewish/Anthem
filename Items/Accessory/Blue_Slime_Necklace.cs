aa8793edf2ad1dc9c2482cfac122cf20412d98e6
JewishLewish <lanbrokhman@gmail.com>
2023-05-13 19:09:51 -0700

    Blue Slime Necklace

---
+++ Items/Accessory/Blue_Slime_Necklace.cs
@@ -0,0 +1,41 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace Anthem.Items.Accessory
+{
+    
+    internal class Blue_Slime_Necklace : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            DisplayName.SetDefault("Blue Slime Necklace"); // The displayed name of the item
+            //Tooltip.SetDefault("150% Faster Movespeed\n500% Damage Increase"); // The tooltip displayed when hovering over the item
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.accessory = true;
+            Item.defense = 2;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.jumpBoost = true; // Enables the jump boost
+            player.jumpSpeedBoost += 0.2f;
+        }
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.Gel, 16) // 16 Gel
+                .AddIngredient(ItemID.IronBar, 4) //4 Iron Bar
+				.AddTile(TileID.WorkBenches) // AddTile takes the TileID
+				.Register(); // Register registers the item
+		}
+    }
+}
