15f2b9d89cdb00251ca1ccfef170c76866ce62ec
JewishLewish <lanbrokhman@gmail.com>
2023-05-13 18:50:25 -0700

    Test

---
+++ Anthem.cs
@@ -0,0 +1,8 @@
+using Terraria.ModLoader;
+
+namespace Anthem
+{
+	public class Anthem : Mod
+	{
+	}
+}
---
+++ Items/FirstSword.cs
@@ -0,0 +1,40 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using Terraria.GameContent.Creative;
+
+namespace Anthem.Items
+{
+    //[AutoloadEquip(EquipType.Shoes)]
+    internal class FirstSword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+            DisplayName.SetDefault("Yeezus"); // The displayed name of the item
+            Tooltip.SetDefault("150% Faster Movespeed\n500% Damage Increase"); // The tooltip displayed when hovering over the item
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 20;
+            Item.height = 20;
+            Item.accessory = true;
+            Item.defense = 5;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.moveSpeed *= 1.5f; // %150 Increase in Speed
+            player.GetDamage(DamageClass.Generic) *= 5f; // %500 Increase in Damage
+        }
+
+		public override void AddRecipes()
+		{
+			CreateRecipe()
+				.AddIngredient(ItemID.Gel, 1) // AddIngredient takes ItemID, then Quantity
+				.AddTile(TileID.WorkBenches) // AddTile takes the TileID
+				.Register(); // Register registers the item
+		}
+    }
+}
