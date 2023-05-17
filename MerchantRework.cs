24a82e87676806c31b473828dd23dd8ea66804cd
JewishLewish <lanbrokhman@gmail.com>
2023-05-16 20:34:10 -0700

    Merchant rework

--- MerchantRework.cs
+++ MerchantRework.cs
@@ -10,11 +10,57 @@ namespace Anthem
 		public override void SetupShop(int type, Chest shop, ref int nextSlot) {
             if (type == NPCID.Merchant) // Check if the NPC is the Merchant
             {
-                int gemOfGrass = nextSlot++;
-                shop.item[gemOfGrass].SetDefaults(ModContent.ItemType<Items.Accessory.GemOfGrass>(), false);
-                shop.item[gemOfGrass].shopCustomPrice = 100000;
+                if (HasItemInInventory(ModContent.ItemType<Items.Accessory.GemOfGrass>())) {
+                    int item = nextSlot++;
+                    shop.item[item].SetDefaults(ModContent.ItemType<Items.Accessory.GemOfGrass>(), false);
+                    shop.item[item].shopCustomPrice = 100000;
+                }
+
+                if (HasItemInInventory(ModContent.ItemType<Items.Accessory.GemOfHeavenBound>())) {
+                    int item = nextSlot++;
+                    shop.item[item].SetDefaults(ModContent.ItemType<Items.Accessory.GemOfHeavenBound>(), false);
+                    shop.item[item].shopCustomPrice = 100000;
+                }
+
+                if (HasItemInInventory(ModContent.ItemType<Items.Accessory.GemOfHellbent>())) {
+                    int item = nextSlot++;
+                    shop.item[item].SetDefaults(ModContent.ItemType<Items.Accessory.GemOfHellbent>(), false);
+                    shop.item[item].shopCustomPrice = 100000;
+                }
+
+                if (HasItemInInventory(ModContent.ItemType<Items.Accessory.GemOfLove>())) {
+                    int item = nextSlot++;
+                    shop.item[item].SetDefaults(ModContent.ItemType<Items.Accessory.GemOfLove>(), false);
+                    shop.item[item].shopCustomPrice = 100000;
+                }
+
+                if (HasItemInInventory(ModContent.ItemType<Items.Accessory.GemOfStrength>())) {
+                    int item = nextSlot++;
+                    shop.item[item].SetDefaults(ModContent.ItemType<Items.Accessory.GemOfStrength>(), false);
+                    shop.item[item].shopCustomPrice = 100000;
+                }
+
+                if (HasItemInInventory(ModContent.ItemType<Items.Accessory.InfinityGauntlet>())) {
+                    int item = nextSlot++;
+                    shop.item[item].SetDefaults(ModContent.ItemType<Items.Accessory.InfinityGauntlet>(), false);
+                    shop.item[item].shopCustomPrice = 600000;
+                }
+
 
             }
 		}
+
+        public bool HasItemInInventory(int itemId)
+        {
+            Player player = Main.LocalPlayer;
+            for (int i = 0; i < player.inventory.Length; i++)
+            {
+                if (player.inventory[i].type == itemId)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
 	}
 }
